ae2d52d02aa1f7e73e73d97a0281de316572e06a
AxeltheDino <168503487+AxeltheDino@users.noreply.github.com>
2025-02-06 13:08:50 +0800

    ScrollView

---
+++ activity1/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+const logo = {
+  width: 64,
+  height: 64,
+};
+
+const App = () => (
+  <ScrollView>
+    <Text style={{fontSize: 96}}>Dan</Text>
+
+    <Text style={{fontSize: 96}}>Axel</Text>
+
+    <Text style={{fontSize: 96}}>Torren</Text>
+
+    <Text style={{fontSize: 96}}>Global</Text>
+
+    <Text style={{fontSize: 96}}>Reciprocal</Text>
+
+    <Text style={{fontSize: 80}}>Colleges</Text>
+  </ScrollView>
+);
+
+export default App;
